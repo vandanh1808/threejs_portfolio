18f7d82a03966a89c4f7cd4c32efa4584d4191af
*tFiTIPc0oq <phamdanh@v-takeuchi.vn>
2025-06-06 16:51:23 +0700

    create about section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -7,6 +7,11 @@ import { Leva, useControls } from "leva";
 import { useMediaQuery } from "react-responsive";
 import { calculateSizes } from "../constants";
 import Target from "../components/Target";
+import ReactLogo from "../components/ReactLogo";
+import Cube from "../components/Cube";
+import Rings from "../components/Rings";
+import HeroCamera from "../components/HeroCamera";
+import Button from "../components/Button";
 
 const Hero = () => {
 	const isSmall = useMediaQuery({ maxWidth: 440 });
@@ -18,24 +23,32 @@ const Hero = () => {
 	return (
 		<section className="min-h-screen w-full flex flex-col relative">
 			<div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-sapce gap-3">
-				<p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
+				<h1 className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">
 					Hi, I am Danh <span className="waving-hand">✌️</span>
 					<p className="hero_tag text-gray_gradient">
 						Building Products & Brands
 					</p>
-				</p>
+				</h1>
 			</div>
-			<d className="w-full h-full absolute inset-0">
+			<div className="w-full h-full absolute inset-0">
 				<Canvas className="w-full h-full">
 					<Suspense fallback={<CanvasLoader />}>
 						<PerspectiveCamera makeDefault position={[0, 0, 30]} />
-						<HackerRoom
-							position={sizes.deskPosition}
-							rotation={[0.4, -3.2, 0]}
-							scale={sizes.deskScale}
-						/>
+						<HeroCamera isMobile={isMobile}>
+							<HackerRoom
+								position={sizes.deskPosition}
+								rotation={[0, -Math.PI, 0]}
+								scale={sizes.deskScale}
+							/>
+						</HeroCamera>
 						<group>
-							<Target position={sizes.targetPosition} />
+							<Target
+								scale={1.5}
+								position={sizes.targetPosition}
+							/>
+							<ReactLogo position={sizes.reactLogoPosition} />
+							<Cube position={sizes.cubePosition} />
+							<Rings position={sizes.ringPosition} />
 						</group>
 						<ambientLight intensity={1} />
 						<directionalLight
@@ -44,7 +57,16 @@ const Hero = () => {
 						/>
 					</Suspense>
 				</Canvas>
-			</d>
+			</div>
+			<div className="absolute bottom-7 left-0 right-0 w-full z-10 c-space">
+				<a href="#contact" className="w-fit">
+					<Button
+						name={`Let's work together`}
+						isBeam
+						containerClass="sm:w-fit w-full sm:min-w-96"
+					/>
+				</a>
+			</div>
 		</section>
 	);
 };
