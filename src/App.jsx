c92e0e9ebf8fd0beae43bb8f3e2d565cda708774
Danh Pham <danhskipper18@gmail.com>
2025-06-09 07:15:17 +0700

    create project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import About from "./sections/About";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
+import Project from "./sections/Project";
 
 function App() {
-	return (
-		<main className="max-w-7xl mx-auto">
-			<Navbar />
-			<Hero />
-			<About />
-		</main>
-	);
+  return (
+    <main className="mx-auto max-w-7xl">
+      <Navbar />
+      <Hero />
+      <About />
+      <Project />
+    </main>
+  );
 }
 
 export default App;
